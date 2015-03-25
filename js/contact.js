fe27027073e24230bcbc7d51c34e33d235910c80
Stephen Milligan (aka Spike) <spikefu@gmail.com>
2015-03-25 11:40:57 -0700

    updated mail/contact url.

--- js/contact.js
+++ js/contact.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact.php",
+                url: "//api.yellowbadger.com/mail/contact",
                 type: "POST",
                 data: {
                     name: name,
